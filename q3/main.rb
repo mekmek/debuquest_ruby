8ee0792241979b66c637367567b89f8b31b5c2ab
mekmek <kawasaki.or4nek@gmail.com>
2022-02-23 19:55:33 +0900

    引数の誤りを修正（あえて文字列に修正）

--- q3/main.rb
+++ q3/main.rb
@@ -4,7 +4,7 @@ require "./q3/sample_api.rb"
 class Q3::Main < Base
   def self.execute
     client = Q3::SampleApi.new
-    base_info = client.get_sample_user_info(type: :default)
+    base_info = client.get_sample_user_info(type: "base")
     detail_info = client.get_sample_user_info(type: :detail)
 
     user_info = base_info
