1540af4816c09fcf8506f57d388b321b11b54bca
mekmek <kawasaki.or4nek@gmail.com>
2022-02-23 19:57:02 +0900

    シンボルをキーとして作成したハッシュは、シンボルをキーとして呼び出す

--- q3/main.rb
+++ q3/main.rb
@@ -8,8 +8,8 @@ class Q3::Main < Base
     detail_info = client.get_sample_user_info(type: :detail)
 
     user_info = base_info
-    user_id_result = "対象のユーザーの id は #{user_info["id"]} です。"
-    user_country_result = "対象のユーザーの country は #{user_info["country"]} です。"
+    user_id_result = "対象のユーザーの id は #{user_info[:id]} です。"
+    user_country_result = "対象のユーザーの country は #{detail_info[:country]} です。"
 
     puts user_id_result
     puts user_country_result
