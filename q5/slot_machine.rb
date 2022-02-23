123596aaa0f8fd1e34eda279970e17293e34e987
mekmek <kawasaki.or4nek@gmail.com>
2022-02-23 21:17:35 +0900

    同じ数字の個数を判定するロジックを修正

--- q5/slot_machine.rb
+++ q5/slot_machine.rb
@@ -16,7 +16,7 @@ class Q5::SlotMachine
     same_numbers_count = count_same_numbers(number1: number1, number2: number2, number3: number3)
     puts "結果: |#{number1}|#{number2}|#{number3}|"
     case same_numbers_count
-    when 1
+    when 0
       puts "残念！ハズレ！"
     when 2
       puts "惜しい！"
@@ -29,9 +29,9 @@ class Q5::SlotMachine
 
     def count_same_numbers(number1:, number2:, number3:)
       same_numbers_count = if (number1 == number2 && number1 == number3)
-                            2
+                            3
                           elsif number1 == number2 || number1 == number3 || number2 == number3
-                            1
+                            2
                           else
                             0
                           end
